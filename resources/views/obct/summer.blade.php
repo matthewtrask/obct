e60d9f25d582c0b835b433f0b1aece5e771001ad
matt trask <matthewtrask@matts-air.lan>
2024-01-15 16:58:34 -0600

    updated summer page

--- resources/views/obct/summer.blade.php
+++ resources/views/obct/summer.blade.php
@@ -3,7 +3,12 @@
 @section('title', 'Summer Sessions')
 
 @section('content')
-    <h2>{{date('Y')}} Summer Sessions</h2>
+    <h2> {{ date('Y') }} Summer Sessions</h2>
+
+    <div data-alert class="alert-box alert" id="alert">
+        <h4>Do not pay until the first day of camp!</h4>
+        <a href="#" class="close">&times;</a>
+    </div>
 
     @foreach($summer as $show)
         <div class="row">
@@ -11,16 +16,15 @@
                 <div class="panel">
                     <div class="row">
                         <div class="small-8 medium-8 columns">
-                            <h4> {{ $show->show_title }} </h4>
-                            <br>
-                            <img height="300px" width="300px" src="{{ $show->show_image }}">
-                            <br><br>
-                            <p><b>Description</b>: {!! $show->show_info !!}</p>
+                            <h4> {{ $show->show_title }} </h4><br>
+                            <img height="300px" width="300px" src="{{ $show->show_image }}"><br><br>
+                            <p><b>Description</b>: {{ $show->show_info }}</p>
                         </div>
                         <div class="small-4 medium-4 columns">
+                            <p><b>Ages</b>: {{ $show->ages }}</p>
                             <p><b>Camp Cost</b>: ${{$show->cost}}</p>
                             <p><b>Show Dates</b>: {{ $show->show_dates }} </p>
-                            <p><b>Camp Dates</b>: {{ $show->dates }} </p>
+                            <p><b>Camp Dates</b>: {!! $show->dates !!} </p>
                             <p><b>Camp Times</b>: {{ $show->time }}</p>
                             <div id="register">
                                 <button class="button register expand"><a href="{{$show->show_link}}">Register</a></button>
